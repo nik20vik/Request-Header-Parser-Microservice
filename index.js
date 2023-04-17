4cf32aea77233cbe774d342ecd4500db4a0f1ec5
nik20vik <nverma20021998@gmail.com>
2023-04-17 19:54:08 +0530

    update route

--- index.js
+++ index.js
@@ -19,9 +19,15 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+const os = require("os");
+
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
 });
 
 // listen for requests :)
